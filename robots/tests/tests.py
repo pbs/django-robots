aea988a8e09758859a5eb4b2013de54d3bcdd55f
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2014-04-28 18:30:06 +0300

    LUN-1413: Update tests.py

--- robots/tests/tests.py
+++ robots/tests/tests.py
@@ -1,9 +1,16 @@
-from django.test import TestCase
-from robots.helpers import get_choices
-from robots.models import Url, Rule
-from cms.api import create_page
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
+from django.utils.functional import partition
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from robots.helpers import get_choices
+from robots.models import Url, Rule
+
+from cms.api import create_page
+
+def second((head_, tail_)):
+    return tail_
 
 
 class TestRules(TestCase):
@@ -19,6 +26,14 @@ class TestRules(TestCase):
         page4 = create_page('page4', 'test.html', 'en', slug='page4', site=site, published=True)
         page41 = create_page('page4_1', 'test.html', 'en', slug='page41', site=site, parent=page4, published=True)
         page42 = create_page('page4_2', 'test.html', 'en', slug='page42', site=site, parent=page4, published=True)
+        self.pages = [
+            page1,
+            page2,
+            page3,
+            page4,
+            page41,
+            page42,
+        ]
 
     def _get_superuser(self):
         admin = User(username="admin", is_staff=True, is_active=True, is_superuser=True)
@@ -27,14 +42,13 @@ class TestRules(TestCase):
         return admin
 
     def test_choices_no_existing_disallowed_rule(self):
-
         choices = get_choices(self.site, 'http')
-
-        self.assertItemsEqual(choices, \
-            [[1, '/admin/'], ['disallowed_0', u'/'],
-             ['disallowed_1', u'/page2/'], ['disallowed_2', u'/page3/'],
-             ['disallowed_3', u'/page4/'], ['disallowed_4', u'/page4/page41/'],
-             ['disallowed_5', u'/page4/page42/']])
+        urls = [reverse('admin:index')] + map(
+            lambda page: page.get_absolute_url(),
+            self.pages
+        )
+        for url in urls:
+            self.assertIn(url, map(second, choices))
 
     def test_choices_existing_disallowed_rules(self):
         rule = Rule.objects.create(robot='*')
@@ -44,12 +58,17 @@ class TestRules(TestCase):
         rule.disallowed.add(url1)
 
         choices = get_choices(self.site, 'http')
-        self.assertItemsEqual(choices, \
-            [[3, '/admin/'], [1, u'/default/'], [2, u'/page2/'],
-             ['disallowed_0', u'/'], ['disallowed_1', u'/page3/'],
-             ['disallowed_2', u'/page4/'], ['disallowed_3', u'/page4/page41/'],
-             ['disallowed_4', u'/page4/page42/']])
+        existing, non_existing = partition(
+            lambda (option, pattern): option.startswith('disallowed'),
+            choices
+        )
 
+        self.assertIn([str(url2.pk), url2.pattern], existing)
+        self.assertIn('/admin/', map(second, non_existing))
+        remaining_pages = self.pages[0:0] + self.pages[2:]
+        for page in remaining_pages:
+            self.assertIn(page.get_absolute_url(), map(second, non_existing))
+        
     def test_selection_boxes_add_rule_admin_selected_by_default(self):
         su = self._get_superuser()
         self.client.login(username='admin', password='admin')
@@ -58,20 +77,20 @@ class TestRules(TestCase):
         #/admin/ is selected by default
         selection_tag = \
 """ <select multiple="multiple" class="selectfilter" name="disallowed" id="id_disallowed">
-<option value="disallowed_0">/</option>
+<option value="disallowed_0" selected="selected">/admin/</option>
 <option value="disallowed_1">/page2/</option>
-<option value="disallowed_2">/page3/</option>
-<option value="disallowed_3">/page4/</option>
-<option value="disallowed_4">/page4/page41/</option>
-<option value="disallowed_5">/page4/page42/</option>
-<option value="1" selected="selected">/admin/</option>
+<option value="disallowed_2">/page4/page41/</option>
+<option value="disallowed_3">/page3/</option>
+<option value="disallowed_4">/page4/page42/</option>
+<option value="disallowed_5">/</option>
+<option value="disallowed_6">/page4/</option>
 </select>"""
-
-        self.assertTrue(selection_tag in response.content)
+        self.assertIn(selection_tag, response.content)
 
     def test_selection_boxes_existing_rule(self):
         rule = Rule.objects.create(robot='*')
         rule.sites.add(self.site)
+        rule.save()
         url1 = Url.objects.create(pattern='/admin/')
         url2 = Url.objects.create(pattern='/default/')
         url3 = Url.objects.create(pattern='/page2/')
@@ -84,14 +103,12 @@ class TestRules(TestCase):
 
         selection_tag = \
 """<select multiple="multiple" class="selectfilter" name="disallowed" id="id_disallowed">
-<option value="3" selected="selected">/page2/</option>
-<option value="disallowed_0">/</option>
-<option value="disallowed_1">/page3/</option>
-<option value="disallowed_2">/page4/</option>
-<option value="disallowed_3">/page4/page41/</option>
-<option value="disallowed_4">/page4/page42/</option>
 <option value="1" selected="selected">/admin/</option>
-<option value="2" selected="selected">/default/</option>
+<option value="3" selected="selected">/page2/</option>
+<option value="disallowed_0">/page4/page41/</option>
+<option value="disallowed_1">/page3/</option>
+<option value="disallowed_2">/page4/page42/</option>
+<option value="disallowed_3">/</option>
+<option value="disallowed_4">/page4/</option>
 </select>"""
-
-        self.assertTrue(selection_tag in response.content)
+        self.assertIn(selection_tag, response.content)
