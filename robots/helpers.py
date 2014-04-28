dec0b88065b8bc53c7103c11ed148bb11b4786ed
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2014-04-28 14:33:40 +0300

    LUN-1413: Change site_url get_choices.

--- robots/helpers.py
+++ robots/helpers.py
@@ -1,12 +1,15 @@
-from django.conf import settings
-from robots.models import Url
-from robots.settings import ADMIN
-from django.db.models import Q
 from itertools import chain
 from itertools import ifilterfalse, imap, izip
-from django.utils.functional import memoize
-from django.utils.importlib import import_module
+from contextlib import contextmanager
 
+from django.conf import settings
+from django.db.models import Q
+from django.utils.functional import memoize, partition
+from django.utils.importlib import import_module
+from django.contrib.sites.models import Site
+
+from robots.models import Url
+from robots.settings import ADMIN
 
 ID_PREFIX = 'disallowed'
 
@@ -26,52 +29,7 @@ def get_url(pattern):
         return Url.objects.filter(pattern=pattern)[0]
 
 
-def get_choices(site, protocol):
-    """
-    Returns a list with the urls patterns for the site parameter
-    The list will be in this format required by the disallowed field widget:
-    [['1', '/pattern1/'], ['2', '/pattern2/'], ['disallowed_3', '/pattern4/'], ...]
-
-    The patterns are taken from the sitemap for the site param.
-    Some of the ids are real db ids, and others (like disallowed_3) are fake ones
-    (generated here).
-    """
-    def get_slug(url):
-        return url['location'].replace("%s://%s" % (protocol, site.domain), '')
-
-    # Make sure that the '/admin/' pattern is allways present
-    #  in the choice list
-    get_url(ADMIN)
-
-    #generate patterns from the sitemap
-    saved_site = settings.__class__.SITE_ID.value
-    settings.__class__.SITE_ID.value = site.id
-    urls = get_sitemap(site=site, protocol=protocol)
-    all_sitemap_patterns = map(get_slug, urls)
-    settings.__class__.SITE_ID.value = saved_site
-
-    #Some patterns are already present in the db and I need their real ids
-    #This processing step could have been avoided, but I need the sitemap
-    #    patterns to be displayed first in the left side box.
-    f = Q(pattern__in=all_sitemap_patterns)
-    db_sitemap_urls = Url.objects.filter(f).values_list('id', 'pattern').distinct()
-    db_sitemap_patterns = map(lambda url:url[1], db_sitemap_urls)
-
-    # Generate some fake ids for the patterns that were not
-    #  previously saved in the db
-    remaining_sitemap_patterns = ifilterfalse(lambda x: x in db_sitemap_patterns, all_sitemap_patterns)
-    fake_ids = imap(lambda x: '%s_%d' % (ID_PREFIX, x), range(len(urls)))
-
-    db_remaining_urls = Url.objects.exclude(f).values_list('id', 'pattern').distinct()
-
-    #returns a list of ['id', 'pattern'] pairs
-    return imap(lambda x: list(x),
-               chain(db_sitemap_urls,
-                     izip(fake_ids, remaining_sitemap_patterns),
-                     db_remaining_urls))
-
-
-def get_sitemap_xml(root=None):
+def get_sitemap_from_urlpatterns(root=None):
     """
     Performs a breadth-first search through the url patterns defined in ROOT_URLCONF
     to find sitemap.xml slug
@@ -87,18 +45,15 @@ def get_sitemap_xml(root=None):
             return urlpattern.default_args
 
     for urlpattern in url_patterns:
-        res = get_sitemap_xml(urlpattern)
+        res = get_sitemap_from_urlpatterns(urlpattern)
         if res:
             return res
     return {}
 
 
-_resolver_cache = {}
-get_sitemap_xml = memoize(get_sitemap_xml, _resolver_cache, 1)
-
-
 def get_sitemap(site, protocol):
-    sitemaps = get_sitemap_xml().get('sitemaps', {})
+    urlpatterns = get_sitemap_from_urlpatterns()
+    sitemaps = urlpatterns.get('sitemaps', {})
     urls = []
     for sitemap in sitemaps.values():
         try:
@@ -121,4 +76,100 @@ def get_sitemap(site, protocol):
         except:
             pass
 
-    return urls
+    locations = map(
+        lambda item: urlparse.urlparse(item['location']).path,
+        urls
+    )
+    
+    return locations
+
+
+# methinks this is better
+
+import xml.etree.ElementTree
+import urllib2
+import urlparse
+
+from django.core.urlresolvers import reverse
+from django.utils.encoding import force_unicode
+
+def fetch_resource(url):
+    opener = urllib2.urlopen(url)
+    data = opener.read()
+    return data
+
+
+def xml_reader(xml_string):
+    tree = xml.etree.ElementTree.fromstring(xml_string)
+    return tree
+
+def fetch_sitemap_locations(site, protocol='http'):
+    scheme, netloc, path = protocol, site.domain, '/sitemap.xml'
+    url = urlparse.urlunparse((scheme, netloc, path, '', '', ''))
+    data = fetch_resource(url)
+    document = xml_reader(data)
+    location_tag = '{http://www.sitemaps.org/schemas/sitemap/0.9}loc'
+    url_nodes = document.iter(location_tag)
+    urls = map(lambda node: node.text, url_nodes)
+    relative_urls = map(
+        lambda url: urlparse.urlparse(url).path,
+        urls
+    )
+    return relative_urls
+
+
+def head2unicode((head,tail)):
+    return [force_unicode(head), tail]
+
+
+def get_choices(site, protocol='http'):
+    """
+    Returns a list with the urls patterns for the site parameter
+    The list will be in this format required by the disallowed field widget:
+    [['1', '/pattern1/'], ['2', '/pattern2/'], ['disallowed_3', '/pattern4/'], ...]
+
+    The patterns are taken from the sitemap for the site param.
+    Some of the ids are real db ids, and others (like disallowed_3) are fake ones
+    (generated here).
+    """
+    admin_url = reverse('admin:index')
+    
+    def fetch_urls():
+        return get_sitemap(site, protocol)
+     
+    with patch(settings, fetch_urls, SITE_ID=site.pk) as result:
+        sitemap_urls = result
+    site_urls = [admin_url] + sitemap_urls
+    in_site_urls = Q(pattern__in=set(site_urls))
+    existing_url_pairs = Url.objects.filter(in_site_urls).values_list('pk', 'pattern')
+    url_patterns = set(map(lambda (_, url): url, existing_url_pairs))
+    non_existing, existings = partition(
+        lambda url: url in url_patterns,
+        set(site_urls)
+    )
+    non_existing_options = map(
+        lambda (number, url): ['disallowed_' + str(number), url],
+        enumerate(non_existing)
+    )
+    existing_options = map(head2unicode, existing_url_pairs)
+    options = existing_options + non_existing_options
+    return options
+
+
+
+@contextmanager
+def patch(instance, fn, **kwargs):
+    def save():
+        return {
+            key: getattr(instance.__class__, key, None)
+            for key, _ in kwargs.items()
+        }
+
+    def set_state(state):
+        for key, value in state.items():
+            setattr(instance.__class__, key, value)
+
+    original = save()
+    set_state(kwargs)
+    yield fn()
+    set_state(original)
