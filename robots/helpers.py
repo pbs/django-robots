058bb0c5dd37787f1ab432a6040f56a0e1f7fd64
Laura Feier <laura.feier@3pillarglobal.com>
2014-07-16 20:29:22 +0300

    LUN-1413: fixed disallowed choices list.

--- robots/helpers.py
+++ robots/helpers.py
@@ -1,127 +1,110 @@
-from itertools import chain
-from itertools import ifilterfalse, imap, izip
-from contextlib import contextmanager
 import urlparse
+import re
 
-from django.conf import settings
-from django.db.models import Q
-from django.utils.functional import memoize, partition
-from django.utils.importlib import import_module
+from django.db import router
+from django.core import urlresolvers
+from django.core.exceptions import ViewDoesNotExist
 from django.utils.encoding import force_unicode
-from django.contrib.sites.models import Site
-from django.core.urlresolvers import reverse, resolve
 
 from robots.models import Url
-from robots.settings import EXCLUDE_URL_NAMES
+from robots.settings import EXCLUDE_URL_NAMES, SITEMAP_VIEWS
 
 
 def get_url(pattern):
+    manager = Url.objects.db_manager(router.db_for_write(Url))
     try:
-        return Url.objects.get_or_create(pattern=pattern)[0]
+        return manager.get_or_create(pattern=pattern)[0]
     except Url.MultipleObjectsReturned:
-        return Url.objects.filter(pattern=pattern)[0]
+        return manager.filter(pattern=pattern)[0]
 
 
-def get_sitemap_from_urlpatterns(root=None):
+def get_view_urlpattern_data(view_name):
+    resolver = urlresolvers.get_resolver(None)
+    lookup_dict = resolver.reverse_dict
+    try:
+        view_data = (lookup_dict.get(view_name) or
+                     lookup_dict.get(urlresolvers.get_callable(view_name)))
+    except ViewDoesNotExist:
+        view_data = None
+    # see django/core/urlresolvers: RegexURLResolver._populate
+    #   view_data: bits, pattern, default_args
+    return view_data
+
+
+def get_sitemaps_from_views():
     """
-    Performs a breadth-first search through the url patterns defined in ROOT_URLCONF
-    to find sitemap.xml slug
+    Fetches sitemaps provided as default arguments to the sitemap view.
     """
-    if not root:
-        mod = import_module(settings.ROOT_URLCONF)
-        url_patterns = mod.urlpatterns
-    else:
-        url_patterns = getattr(root, 'url_patterns', [])
-
-    for urlpattern in url_patterns:
-        if 'sitemap.xml' in urlpattern.regex.pattern:
-            return urlpattern.default_args
-
-    for urlpattern in url_patterns:
-        res = get_sitemap_from_urlpatterns(urlpattern)
-        if res:
-            return res
-    return {}
+    sitemaps = []
+    for view_name in SITEMAP_VIEWS:
+        view_data = get_view_urlpattern_data(view_name)
+        if not view_data:
+            continue
+        bits, pattern, default_args = view_data
+        sitemaps += (default_args.get('sitemaps') or {}).values()
+    return sitemaps
 
 
-def get_sitemap(site, protocol):
-    urlpatterns = get_sitemap_from_urlpatterns()
-    sitemaps = urlpatterns.get('sitemaps', {})
+def get_paths_from_sitemaps(site, protocol):
+    """
+    Returns a list of paths fetched from all sitemaps items location.
+    """
     urls = []
-    for sitemap in sitemaps.values():
+    for sitemap in get_sitemaps_from_views():
         try:
             if callable(sitemap):
                 sitemap = sitemap()
             urls.extend(sitemap.get_urls(site=site, protocol=protocol))
         except:
             pass
-    locations = map(
-        lambda item: urlparse.urlparse(item['location']).path,
-        urls
-    )
-    return locations
+    return map(lambda item: urlparse.urlparse(item['location']).path, urls)
 
 
-def head2unicode((head,tail)):
+def head2unicode((head, tail)):
     return [force_unicode(head), tail]
+
+
+exclude_patterns = []
 
 
 def get_choices(site, protocol='http'):
     """
-    Returns a list with the urls patterns for the site parameter
-    The list will be in this format required by the disallowed field widget:
-    [['1', '/pattern1/'], ['2', '/pattern2/'], ['disallowed_3', '/pattern4/'], ...]
-
-    The patterns are taken from the sitemap for the site param.
-    Some of the ids are real db ids, and others (like disallowed_3) are fake ones
-    (generated here).
+        Returns a list of choices(id, pattern pairs) for Url objects
+    that have patterns generated from sitemaps.
     """
-    admin_url = reverse('admin:index')
+    if EXCLUDE_URL_NAMES and not exclude_patterns:
+        exclude_patterns = map(lambda view: get_view_urlpattern_data(view)[1],
+                               EXCLUDE_URL_NAMES)
+
+    def should_exclude(url):
+        """
+        Checks to see if a url matches any of the patterns from views
+        specified by EXCLUDE_URL_NAMES.
+        """
+        return any((re.search(patt, url)
+                    for patt in exclude_patterns)) is True
 
     def fetch_urls():
-        urls = [
+        """
+        Returns a set of url paths fetched from sitemaps.
+        """
+        return set(
             url
-            for url in get_sitemap(site, protocol)
-            if resolve(url).url_name not in EXCLUDE_URL_NAMES
-        ]
-        return urls
+            for url in get_paths_from_sitemaps(site, protocol)
+            if not should_exclude(url)
+        )
 
-    with patch(settings, fetch_urls, SITE_ID=site.pk) as result:
-        sitemap_urls = result
-    site_urls = [admin_url] + sitemap_urls
-    in_site_urls = Q(pattern__in=set(site_urls))
-    existing_url_pairs = Url.objects.filter(in_site_urls).values_list('pk', 'pattern')
-    url_patterns = set(map(lambda (_, url): url, existing_url_pairs))
-    non_existing, existings = partition(
-        lambda url: url in url_patterns,
-        set(site_urls)
-    )
-    non_existing_options = map(
-        lambda (number, url): ['disallowed_' + str(number), url],
-        enumerate(non_existing)
-    )
-    existing_options = map(head2unicode, existing_url_pairs)
-    options = sorted(
-        existing_options + non_existing_options,
-        key=lambda (_, pattern): pattern
-    )
-    return options
+    site_urls = fetch_urls()
+    manager = Url.objects.db_manager(router.db_for_write(Url))
 
+    existing_urls_qs = manager.filter(pattern__in=site_urls)\
+        .values_list('pk', 'pattern')
 
-
-@contextmanager
-def patch(instance, fn, **kwargs):
-    def save():
-        return {
-            key: getattr(instance.__class__, key, None)
-            for key, _ in kwargs.items()
-        }
-
-    def set_state(state):
-        for key, value in state.items():
-            setattr(instance.__class__, key, value)
-
-    original = save()
-    set_state(kwargs)
-    yield fn()
-    set_state(original)
+    existing_patterns = set(map(lambda (_, pattern): pattern,
+                                existing_urls_qs))
+    # generate url objects
+    manager.bulk_create([Url(pattern=url)
+                         for url in site_urls
+                         if url not in existing_patterns])
+    # re-evaluate queryset to fetch latest data
+    return map(head2unicode, existing_urls_qs.order_by('pattern'))
